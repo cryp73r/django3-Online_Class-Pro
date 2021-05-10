b24a0cdaf6722f10584eb06728111235da53f4e4
Priyanshu Singh <priyanshusingh0610@gmail.com>
2021-05-11 01:33:24 +0530

    App Update v2.2.5

--- classdetail/api_call_ver.py
+++ classdetail/api_call_ver.py
@@ -1,6 +1,6 @@
 import json
 
 def apiCallVer():
-    raw_json = {"ver": "v2.2.5","size": "19.3MB" , "url": "https://mega.nz/file/6Jl0HBaA#RkvRsgWN3uuKg9ifsZKxGuXWlNl0Ip0gI_4K5wpztoc"}
+    raw_json = {"ver": "v2.2.5","size": "17.9MB" , "url": "https://mega.nz/file/bYlFEY5T#d3eaxfQ4_PCMTHmPu0qOC84xjgCYiuvrqnBFRhIMSlc"}
     with open('classdetail/templates/classdetail/json/classdetails/raw_data_ver.json', 'w') as outfile:
         json.dump(raw_json, outfile)
