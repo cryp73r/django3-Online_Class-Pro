a1602be5c64e94c4290ed201020d88fbb6e5b595
Priyanshu Singh <priyanshusingh0610@gmail.com>
2021-05-23 00:27:10 +0530

    API Support for Notice

--- notice/api_call.py
+++ notice/api_call.py
@@ -3,7 +3,7 @@ from .models import Notice
 
 def apiCall():
     raw_json = {"data": []}
-    noticeData = Notice.objects.all()
+    noticeData = Notice.objects.all().order_by('-date')
     for element in noticeData:
         raw_json["data"].append({
             "department": element.department,
