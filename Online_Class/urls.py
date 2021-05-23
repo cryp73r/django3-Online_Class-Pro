58a8492d26db2464949ab1063dda9cf696041499
Priyanshu Singh <priyanshusingh0610@gmail.com>
2021-05-23 19:36:25 +0000

    Fixed Bugs

--- Online_Class/urls.py
+++ Online_Class/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 from classdetail import views
 
 urlpatterns = [
@@ -41,7 +42,10 @@ urlpatterns = [
     path('notice/', include('notice.urls')),
 
     #API AppRelease
-    path('app/', include('appRelease.urls'))
+    path('app/', include('appRelease.urls')),
+
+    re_path(r'^app/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
